5f60af58f2940fc6377cc01dc0c9c2865826b56e
kaushal-zillion <kaushal.zillioninfotech@gmail.com>
2026-02-16 18:40:24 +0530

    Delete add-to-cart directory

--- add-to-cart/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-    { path: '', loadComponent: () => import('./pages/product-list/product-list').then(m => m.ProductList) },
-    { path: 'cart', loadComponent: () => import('./pages/cart/cart').then(m => m.Cart) },
-];
